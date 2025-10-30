7f4ff098c015ffe2b5ad43e66cb8296d2a8f0a47
davidSaumell <dsegurasmll@gmail.com>
2025-10-30 15:50:42 +0100

    fix: fix repository

---
+++ back/api.py
@@ -0,0 +1,20 @@
+from requestHandler import RequestHandler
+from flask import Flask, jsonify, request as req
+
+app = Flask(__name__)
+
+@app.route("/recommend", methods = ["GET"])
+def recommend():
+    return RequestHandler.recommend
+
+@app.route("/train", methods = ["GET"])
+def train():
+    return RequestHandler.train
+
+@app.route("/version", methods = ["GET"])
+def version():
+    return RequestHandler.version
+
+@app.route("/test", methods = ["GET"])
+def test():
+    return RequestHandler.test
