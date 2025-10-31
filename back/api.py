604230a4d4c8e49bc06c143d0a6dd7b53b70f159
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-10-31 15:54:23 +0100

    fix: api methods

--- back/api.py
+++ back/api.py
@@ -3,18 +3,18 @@ from flask import Flask, jsonify, request as req
 
 app = Flask(__name__)
 
-@app.route("/recommend", methods = ["GET"])
+@app.route("/recommend/", methods = ["GET"])
 def recommend():
-    return RequestHandler.recommend
+    return RequestHandler.recommend()
 
-@app.route("/train", methods = ["PATCH"])
+@app.route("/train/", methods = ["PATCH"])
 def train():
-    return RequestHandler.train
+    return RequestHandler.train()
 
-@app.route("/version", methods = ["GET"])
+@app.route("/version/", methods = ["GET"])
 def version():
-    return RequestHandler.version
+    return RequestHandler.version()
 
-@app.route("/test", methods = ["GET"])
+@app.route("/test/", methods = ["GET"])
 def test():
-    return RequestHandler.test
+    return RequestHandler.test()
