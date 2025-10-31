3d2ac7082b70a14fd352f417da59a5e51cfa1089
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-10-31 15:38:20 +0100

    feat: option selection

--- front/main.py
+++ front/main.py
@@ -1,27 +1,48 @@
-NUMBER_OF_ANIMES_TO_VALORATE = 3
+NUMBER_OF_ANIMES_TO_RATE = 3
 username = input("Escribe tu nombre de usuario: ")
 password = input("Escribe tu contraseña: ")
 
-def check_valoration(valoration):
-    correct_valoration = False
-    if valoration.isnumeric():
-        valoration = int(valoration)
-        if valoration >= 0 and valoration <= 10:
-            correct_valoration = True
+def showMenu():
+    menu = ("\033[33m1.- Obtener recomendaciones.\n"
+        "2.- Entrenar el algoritmo.\n"
+        "3.- Obtener la versión.\n"
+        "4.- Testear el algoritmo.\n"
+        "0.- Apagar programa.\033[0m\n"
+    )
+    return menu
+
+def check_rating(rating):
+    correct_rating = False
+    if rating.isnumeric():
+        rating = int(rating)
+        if rating >= 0 and rating <= 10:
+            correct_rating = True
             
-    return correct_valoration
+    return correct_rating    
 
-user_ratings = {}
-counter = 0
-while counter < NUMBER_OF_ANIMES_TO_VALORATE:
-    # TO-DO: Add check_anime_id
-    anime_id = input("Escriba el id del anime a valorar: ")
+loop = True
+while loop:
+    print(showMenu)
+    option = input("Elija la opción: ")
+    if option == 1:
+        user_ratings = {}
+        counter = 0
+        while counter < NUMBER_OF_ANIMES_TO_RATE:
+            # TODO: Add check_anime_id
+            anime_id = input("Escriba el id del anime a valorar: ")
 
-    correct_valoration = False
-    while correct_valoration == False:
-        valoration = input(f"Escriba la valoración para el anime {anime_id}: ")
-        correct_valoration = check_valoration(valoration)
-        
-    user_ratings[int(anime_id)] = int(valoration)
+            correct_rating = False
+            while correct_rating == False:
+                rating = input(f"Escriba la valoración para el anime {anime_id}: ")
+                correct_rating = check_rating(rating)
+                
+            user_ratings[int(anime_id)] = int(rating)
 
-    counter += 1
+            counter += 1
+
+        # user_ratings
+
+    if option == 0:
+        loop = False
+    else:
+        print("Opción no válida")
