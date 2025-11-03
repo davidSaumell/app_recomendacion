de277767f0474300244b86c4f06ae17e7d6ec5a4
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-11-03 16:28:25 +0100

    feat: read users function

--- back/usersDAO.py
+++ back/usersDAO.py
@@ -5,9 +5,25 @@ class usersDAO:
 
     @staticmethod
     def create_user(user):
-        query = "INSERT INTO users (username, password) VALUES %s, %s"
+        query = "INSERT INTO users (userName, password) VALUES %s, %s"
         cursor = db.connection.cursor()
         cursor.execute(query, user.get_username(), user.get_password())
         db.connection.commit()
 
         return user
+    
+    def read_users():
+        query = "SELECT * FROM users"
+        cursor = db.connection.cursor()
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+        users = []
+        for row in rows:
+            userid = row[0]
+            username = row[1]
+            password = row[2]
+
+            users.append(User(username, password, userid))
+        
+        return users
