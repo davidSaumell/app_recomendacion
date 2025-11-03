759400cb6c0a1c1cd54efd5f4b5d629ebe78490d
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-11-03 16:45:05 +0100

    feat: add filtered read function

--- back/usersDAO.py
+++ back/usersDAO.py
@@ -13,6 +13,24 @@ class usersDAO:
         return user
     
     @staticmethod
+    def read_user(user):
+        query = "SELECT * FROM users WHERE userName = %s AND password = %s"
+        values = (user.get_username(), user.getpassword())
+        cursor = db.connection.cursor()
+        cursor.execute(query, values)
+        
+        rows = cursor.fetchall()
+        users = []
+        for row in rows:
+            userid = row[0]
+            username = row[1]
+            password = row[2]
+
+            users.append(User(username, password, userid))
+        
+        return users
+    
+    @staticmethod
     def read_users():
         query = "SELECT * FROM users"
         cursor = db.connection.cursor()
